04e2d87c7526fa24678bab9d3ce9c99a7135325d
z4pple <saiful0695nizam@gmail.com>
2024-07-02 22:58:11 +0800

    first commit

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import os
 
 #command
 #python -m venv venv
@@ -183,4 +184,5 @@ def predict():
         '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
